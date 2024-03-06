d352b6106cf80fc0e3da64c89d8ecb902fdbfe5c
ztom000 <Qwer1023>
2024-03-06 11:13:01 +0800

    Initial commit

---
+++ test-cloud-authorization-server/src/main/java/org/ztom/cloud/authorization/AuthorizationApplication.java
@@ -0,0 +1,18 @@
+package org.ztom.cloud.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description TODO
+ * @Author 翟涛
+ * @Version 1.0
+ * @Date 2024/3/6 11:05
+ */
+@SpringBootApplication
+public class AuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationApplication.class, args);
+    }
+}
