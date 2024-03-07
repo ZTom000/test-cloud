e6fe0931ce77c838077f978415e9dff633515d14
ztom000 <Qwer1023>
2024-03-07 10:30:14 +0800

    20240307003 1.初始化 gateway 项目

---
+++ test-cloud-gateway/src/main/java/org/ztom/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package org.ztom.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description TODO
+ * @Author 翟涛
+ * @Version 1.0
+ * @Date 2024/3/7 9:37
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
--- test-cloud-resource-server/src/main/java/org/ztom/cloud/resource/ResourceServerApplication.java
+++ test-cloud-resource-server/src/main/java/org/ztom/cloud/resource/ResourceServerApplication.java
@@ -2,6 +2,7 @@ package org.ztom.cloud.resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Description TODO
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version 1.0
  * @Date 2024/3/7 8:36
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ResourceServerApplication {
     public static void main(String[] args) {
